5d9e602453a561e035c84c5ac12ac5b3ef7db008
Jakson Casas <jaksoncasas@gmail.com>
2021-11-27 17:34:38 -0500

    paths

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,8 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [{
-  path:''//Todo: localhost:4200
+  path:'',//Todo: localhost:4200
+  loadChildren:() => import(`./modules/home/home.module`).then(m => m.HomeModule)
 }];
 
 @NgModule({
