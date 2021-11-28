7778f58c23c9f6a4c9ae6d6a25bdee8468a59087
Jakson Casas <jaksoncasas@gmail.com>
2021-11-27 22:35:14 -0500

    Side bar implement arrays

--- src/app/shared/components/side-bar/side-bar.component.ts
+++ src/app/shared/components/side-bar/side-bar.component.ts
@@ -6,10 +6,46 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./side-bar.component.css']
 })
 export class SideBarComponent implements OnInit {
-
+  mainMenu: {defaultOptions: Array<any>, accessLink: Array<any>} = {defaultOptions:[], accessLink:[]}
+  customOptions: Array<any> = []
   constructor() { }
 
   ngOnInit(): void {
+    this.mainMenu.defaultOptions = [
+      {
+        name: 'Home',
+        icon: 'uil uil-estate',
+        router: ['/']
+      },
+      {
+        name: 'Buscar',
+        icon: 'uil uil-search',
+        router: ['/']
+      },
+      {
+        name: 'Tu biblioteca',
+        icon:'uil uil-chart',
+        router:['/']
+      }
+    ]
+    this.customOptions = [
+      {
+        name: 'Mi lista #1',
+        router:['/']
+      },
+      {
+        name: 'Mi lista #2',
+        router:['/']
+      },
+      {
+        name: 'Mi lista #3',
+        router:['/']
+      },
+      {
+        name: 'Mi lista #4',
+        router:['/']
+      }
+    ]
   }
 
 }
