79bf02674b765c35a0e52280b264af2d9b8fb1d0
Jakson Casas <jaksoncasas@gmail.com>
2022-01-01 18:03:22 -0500

    create image default

--- src/app/shared/directives/img-broken.directive.ts
+++ src/app/shared/directives/img-broken.directive.ts
@@ -1,10 +1,17 @@
-import { Directive } from '@angular/core';
+import { Directive, ElementRef, HostListener } from '@angular/core';
 
 @Directive({
-  selector: '[appImgBroken]'
+  selector: 'img[appImgBroken]'
 })
 export class ImgBrokenDirective {
-
-  constructor() { }
+  //Todo: host Host HOST
+  @HostListener('error') handleError(): void {
+    const elNative = this.elHost.nativeElement;
+    console.log('Esta imagen reventro ->', this.elHost);
+    elNative.src = '../../../assets/images/default-placeholder.png'
+  }
+  constructor(private elHost:ElementRef) { 
+    console.log(elHost);
+  }
 
 }
